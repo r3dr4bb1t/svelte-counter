812368e6f4cfe05327e711726906f7d245072f2a
Minsu Roh <minsu.roh@MacBook-Pro.local>
2024-02-19 16:57:02 +0900

    init counter

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let title: string = 'Multiple Counter';
+  let count: number = 0;
+
+  function increment() {
+    count += 1;
+  }
+
+  function decrement() {
+    count -= 1;
+  }
+
+  function reset() {
+    count = 0;
+  }
+</script>
+
+<div>
+  <h2>{title}</h2>
+  <button on:click={decrement}>-</button>
+  <span>{count}</span>
+  <button on:click={increment}>+</button>
+  <button on:click={reset}>Reset</button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Counter from '../lib/Counter.svelte';
+  let counters: Array<{ id: number; title: string }> = [
+    { id: 1, title: 'Counter 1' },
+  ];
+  let nextId = 2;
+
+  function addCounter() {
+    counters = [...counters, { id: nextId++, title: `Counter ${nextId}` }];
+  }
+
+  function removeCounter(id: number) {
+    counters = counters.filter(counter => counter.id !== id);
+  }
+</script>
+
+<button on:click={addCounter}>new counter</button>
+
+{#each counters as { id, title }}
+  <div>
+    <Counter {title} />
+    <button on:click={() => removeCounter(id)}>Remove</button>
+  </div>
+{/each}
